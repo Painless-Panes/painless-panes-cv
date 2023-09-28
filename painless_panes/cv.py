344bd6f5ec01e8668be425cc277be7d25739bfa9
Andreas Copan <avcopan@gmail.com>
2023-09-27 22:11:09 -0400

    WIP save

--- painless_panes/cv.py
+++ painless_panes/cv.py
@@ -1,13 +1,16 @@
+import itertools
 from typing import List, Tuple
 
 import cv2  # opencv
 import numpy
+import skimage.feature
 
-from painless_panes import model
+from painless_panes import model, util
 
 # Module constants
 BLUE = (255, 0, 0)  # annotation color
 GREEN = (0, 255, 0)  # annotation color
+RED = (0, 0, 255)  # annotation color
 FONT = cv2.FONT_HERSHEY_SIMPLEX
 ARUCO_PARAMS = cv2.aruco.DetectorParameters()
 ARUCO_DICT = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_4X4_50)
@@ -96,7 +99,7 @@ def find_window_corners(
 
     :param image: The image
     :type image: numpy.ndarray
-    :param bbox: A bounding box for the window
+    :param bbox: A window bounding box in xyxy format
     :type bbox: List[int]
     :param annotate: Annotate the image file as a side-effect?, defaults to False
     :type annotate: bool, optional
@@ -104,63 +107,149 @@ def find_window_corners(
         [[x1, y1], [x2, y2], [x3, y3], [x4, y4]]
     :rtype: numpy.ndarray
     """
+    bx0, by0, bx1, by1 = bbox
+    bwidth = bx1 - bx0
+    bheight = by1 - by0
+    cx0 = bx0 + 0.1 * bwidth
+    cx1 = bx1 - 0.1 * bwidth
+    cy0 = by0 + 0.5 * bheight
+    cy1 = by1 - 0.5 * bheight
+
+    # Define some helper data
+    edge_keys = {"t", "r", "b", "l"}
+    edge_nkey_dct = {"t": "rl", "b": "rl", "r": "tb", "l": "rb"}
+    corner_keys = {frozenset("tr"), frozenset("br"), frozenset("bl"), frozenset("tl")}
+
+    edges = find_edges_in_bounding_box(image, bbox)
+    corners = find_corners_in_bounding_box(image, bbox)
+    corner_pool = corners.tolist()
+
+    # 7. Separate them into left- and right-sorted vertical lines and top- and
+    # bottom-sorted horizontal lines
+    vedges, hedges = partition_lines_by_orientation(edges)
+
+    edge_idx_pool = {
+        "t": argsort_horizontal_lines(hedges, cutoff=cy0, reverse=False),
+        "r": argsort_vertical_lines(vedges, cutoff=cx1, reverse=True),
+        "b": argsort_horizontal_lines(hedges, cutoff=cy1, reverse=True),
+        "l": argsort_vertical_lines(vedges, cutoff=cx0, reverse=False),
+    }
+
+    for edge_key, edge_idxs in edge_idx_pool.items():
+        print(edge_key)
+        color = numpy.random.randint(0, 255, 3)
+        if edge_key in "tb":
+            edges = hedges[edge_idxs]
+        else:
+            edges = vedges[edge_idxs]
+        points_list = numpy.reshape(edges, (-1, 2, 2))
+        annotate_lines(image, points_list, color=color)
+
+    # for corner in corners:
+    #     cv2.circle(image, numpy.intp(corner), 1, color=RED, thickness=3)
+
+    # # Cutoffs for vertical line positions (10% in from the bounding box frame)
+    # cx0 = bx0 + 0.1 * bwidth
+    # cx1 = bx1 - 0.1 * bwidth
+
+    # side_line1 = side_lines["r"].pop(0)
+    # side_line1 = side_lines["r"].pop(0)
+    # side_line2 = side_lines["b"].pop(0)
+
+    # corner = util.line_pair_ray_intersection(side_line1, side_line2)
+    # annotate_line(image, numpy.reshape(side_line1, (-1, 2)), color=GREEN)
+    # annotate_line(image, numpy.reshape(side_line2, (-1, 2)), color=RED)
+    # cv2.circle(image, numpy.intp(corner), 5, color=BLUE, thickness=3)
+
+    # corners_list = []
+    # corners_iter = itertools.pairwise(itertools.cycle(side_keys))
+    # for side_key1, side_key2 in corners_iter:
+    #     print(side_key1, side_key2)
+    #     if not (side_lines[side_key1] and side_lines[side_key2]):
+    #         break
+    #     side_line1 = side_lines[side_key1].pop(0)
+    #     side_line2 = side_lines[side_key2].pop(0)
+
+    # vpoints_list = numpy.reshape(vlines, (-1, 2, 2))
+    # hpoints_list = numpy.reshape(hlines, (-1, 2, 2))
+    # annotate_lines(image, vpoints_list, color=GREEN)
+    # annotate_lines(image, hpoints_list, color=RED)
+    return image
+
+
+def find_edges_in_bounding_box(image: numpy.ndarray, bbox: List[int]) -> numpy.ndarray:
+    """Find lines that fall within a bounding box
+
+    :param image: The image
+    :type image: numpy.ndarray
+    :param bbox: A window bounding box in xyxy format
+    :type bbox: List[int]
+    :returns: The lines, as an N x 4 array
+    :rtype: numpy.ndarray
+    """
     image = image.copy()
 
-    # 1. Find all edges with canny edge detection
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    image = cv2.GaussianBlur(image, (5, 5), 0)
-    all_edges = cv2.Canny(image, 30, 150)
+    # 1. Convert to grayscale
+    image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-    # 2. Select edges within the bounding box
-    x0, y0, x1, y1 = bbox
-    edges = numpy.zeros_like(all_edges)
-    edges[y0:y1, x0:x1] = all_edges[y0:y1, x0:x1]
+    # 2. Read the bounding box corners and get its width
+    bx0, _, bx1, _ = bbox
+    bwidth = bx1 - bx0
 
-    # 3. Detect straight lines among the edges
-    fld = cv2.ximgproc.createFastLineDetector()
-    lines = fld.detect(edges)
-    # image = fld.drawSegments(image, lines, linecolor=(0, 255, 0))
+    # 3. Detect straight lines
+    fld = cv2.ximgproc.createFastLineDetector(
+        length_threshold=bwidth // 5,
+        do_merge=True,
+    )
+    lines = fld.detect(image_gray)
 
-    # 4. Find the longest
+    # 4. Sort the line points for convenience
+    lines = sort_lines_points(lines, axis="y")
+
+    # 5. Select lines that fall within the bounding box
+    lines = select_lines_in_bounding_box(lines, bbox)
+
+    # 6. Merge the lines by ray
+    lines_groups = util.equivalence_partition(lines, util.lines_fall_on_common_ray)
+    lines = list(map(util.merge_lines, lines_groups))
+
+    return lines
 
 
-def partition_lines_by_orientation(
-    lines: numpy.ndarray, vert_angle: float = 30
-) -> Tuple[numpy.ndarray, numpy.ndarray]:
-    """Partition a set of lines by vertical and horizontal orientation
+def find_corners_in_bounding_box(
+    image: numpy.ndarray, bbox: List[int]
+) -> numpy.ndarray:
+    """Find corners that fall within a bounding box
 
-    Partitions into vertical and horizontal lines based on a vertical angle cutoff
-
-    :param lines: The lines
-    :type lines: numpy.ndarray
-    :param vert_angle: Cutoff for identifying vertical lines, defaults to 30
-    :type vert_angle: float, optional
-    :return: The vertical lines, and the horizontal lines
-    :rtype: Tuple[numpy.ndarray, numpy.ndarray]
+    :param image: The image
+    :type image: numpy.ndarray
+    :param bbox: A window bounding box in xyxy format
+    :type bbox: List[int]
+    :returns: The corner points, as an N x 2 array
+    :rtype: numpy.ndarray
     """
-    vlines = []
-    hlines = []
 
-    print(lines.shape)
-    print("LINES:")
-    for line in lines:
-        (lin,) = line
-        print(line)
-        # Calculate the angle
-        x, y = numpy.subtract(lin[2:], lin[:2])
-        ang = numpy.arctan(x / y)
-        # If the angle is near vertical, add it to `vlines`
-        if abs(ang) < vert_angle or abs(numpy.pi - ang) < vert_angle:
-            vlines.append(line)
-        # Otherwise, add it to `hlines`
-        else:
-            hlines.append(line)
+    # 1. Convert to grayscale
+    image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-    vlines = numpy.array(vlines)
-    hlines = numpy.array(hlines)
-    print(vlines.shape)
-    print(hlines.shape)
-    return vlines, hlines
+    # 2. Read the bounding box corners and get its width
+    bx0, _, bx1, _ = bbox
+    bwidth = bx1 - bx0
+
+    # 3. Corner Harris generates a map where high values are likely to have a corner
+    block_size = max(2, bwidth // 40)
+    corner_map = cv2.cornerHarris(image_gray, blockSize=block_size, ksize=3, k=0.04)
+
+    # 4. Set low values to zero, clearing out small local maxima
+    corner_map[corner_map < 0.01 * corner_map.max()] = corner_map.min()
+
+    # 5. Identify remaining local maxima, which are likely to be corners
+    corners = skimage.feature.peak_local_max(corner_map.T, min_distance=5)
+
+    # 6. Select corners that fall within the bounding box
+    corners = select_points_in_bounding_box(corners, bbox)
+
+    return corners
 
 
 # Selectors
@@ -211,6 +300,189 @@ def select_central_detection(
         annotate_rectangle(image, bbox, text=text)
 
     return central_detection
+
+
+def select_points_in_bounding_box(
+    points: numpy.ndarray,
+    bbox: numpy.ndarray,
+) -> numpy.ndarray:
+    """Select points that fall within a bounding box
+
+    :param points: The lines
+    :type points: numpy.ndarray
+    :param bbox: A bounding box in xyxy format
+    :type bbox: List[int]
+    :return: The points inside the bounding box
+    :rtype: numpy.ndarray
+    """
+    bx0, by0, bx1, by1 = bbox
+
+    print(points.shape)
+    xvals = points[:, 0]
+    yvals = points[:, 1]
+
+    in_bbox = (
+        numpy.greater_equal(xvals, bx0)
+        & numpy.less_equal(xvals, bx1)
+        & numpy.greater_equal(yvals, by0)
+        & numpy.less_equal(yvals, by1)
+    )
+
+    points_in_bbox = points[in_bbox]
+    return points_in_bbox
+
+
+def select_lines_in_bounding_box(
+    lines: numpy.ndarray,
+    bbox: numpy.ndarray,
+) -> numpy.ndarray:
+    """Select lines that fall within a bounding box
+
+    :param lines: The lines
+    :type lines: numpy.ndarray
+    :param bbox: A bounding box in xyxy format
+    :type bbox: List[int]
+    :return: The lines inside the bounding box
+    :rtype: numpy.ndarray
+    """
+    bx0, by0, bx1, by1 = bbox
+
+    xvals = lines[:, 0, 0::2]
+    yvals = lines[:, 0, 1::2]
+
+    in_bbox = (
+        numpy.all(numpy.greater_equal(xvals, bx0), 1)
+        & numpy.all(numpy.less_equal(xvals, bx1), 1)
+        & numpy.all(numpy.greater_equal(yvals, by0), 1)
+        & numpy.all(numpy.less_equal(yvals, by1), 1)
+    )
+
+    lines_in_bbox = lines[in_bbox]
+    return lines_in_bbox
+
+
+def sort_lines_points(
+    lines: numpy.ndarray,
+    axis: str = "y",
+) -> numpy.ndarray:
+    """Sort the two points for each lines along a particular axis
+
+    :param lines: The lines
+    :type lines: numpy.ndarray
+    :param axis: The axis to sort along, defaults to "y"
+    :type axis: str, optional
+    :return: The lines, with points sorted for each
+    :rtype: numpy.ndarray
+    """
+    vals1 = lines[:, 0, 1] if axis == "y" else lines[:, 0, 0]
+    vals2 = lines[:, 0, 3] if axis == "y" else lines[:, 0, 2]
+
+    # Identify rows where the values need to be swapped
+    swap = numpy.greater(vals1, vals2)
+
+    # Copy the array
+    lines_out = lines.copy()
+
+    # Do the swap
+    lines_out[swap, :, :2] = lines[swap, :, 2:]
+    lines_out[swap, :, 2:] = lines[swap, :, :2]
+    return lines_out
+
+
+def partition_lines_by_orientation(
+    lines: numpy.ndarray, vert_angle: float = 30
+) -> Tuple[numpy.ndarray, numpy.ndarray]:
+    """Partition a set of lines by vertical and horizontal orientation
+
+    Partitions into vertical and horizontal lines based on a vertical angle cutoff
+
+    :param lines: The lines
+    :type lines: numpy.ndarray
+    :param vert_angle: Cutoff for identifying vertical lines, in degrees, defaults to 30
+    :type vert_angle: float, optional
+    :return: The vertical lines, and the horizontal lines
+    :rtype: Tuple[numpy.ndarray, numpy.ndarray]
+    """
+    vert_angle *= numpy.pi / 180.0
+
+    vlines = []
+    hlines = []
+
+    for line in lines:
+        ang = util.line_angle(line)
+        # If the angle is near vertical, add it to `vlines`
+        if abs(ang) < vert_angle or abs(numpy.pi - ang) < vert_angle:
+            vlines.append(line)
+        # Otherwise, add it to `hlines`
+        else:
+            hlines.append(line)
+
+    vlines = numpy.array(vlines)
+    hlines = numpy.array(hlines)
+
+    return vlines, hlines
+
+
+def argsort_vertical_lines(
+    vlines: numpy.ndarray, cutoff: float = None, reverse: bool = False
+) -> numpy.ndarray:
+    """Sort vertical lines by x-coordinates
+
+    :param vlines: The vertical lines
+    :type vlines: numpy.ndarray
+    :param cutoff: Drop lines past this cutoff, defaults to None
+    :type cutoff: float, optional
+    :param reverse: Reverse the sort order?, defaults to False
+    :type reverse: bool, optional
+    :return: The sorted lines
+    :rtype: numpy.ndarray
+    """
+    min_x_vals = numpy.min(numpy.squeeze(vlines)[:, 0::2], axis=1)
+    max_x_vals = numpy.max(numpy.squeeze(vlines)[:, 0::2], axis=1)
+
+    if not reverse:
+        vsort_idxs = numpy.argsort(min_x_vals, axis=0)
+        vsort_idxs = list(vsort_idxs)
+        if cutoff is not None:
+            vsort_idxs = [i for i in vsort_idxs if max_x_vals[i] < cutoff]
+    else:
+        vsort_idxs = numpy.argsort(max_x_vals, axis=0)
+        vsort_idxs = list(reversed(vsort_idxs))
+        if cutoff is not None:
+            vsort_idxs = [i for i in vsort_idxs if min_x_vals[i] > cutoff]
+
+    return vsort_idxs
+
+
+def argsort_horizontal_lines(
+    hlines: numpy.ndarray, cutoff: float = None, reverse: bool = False
+) -> numpy.ndarray:
+    """Sort horizontal lines by y-coordinates
+
+    :param lines: The lines
+    :type lines: numpy.ndarray
+    :param cutoff: Drop lines past this cutoff, defaults to None
+    :type cutoff: float, optional
+    :param reverse: Reverse the sort order?, defaults to False
+    :type reverse: bool, optional
+    :return: The sorted lines
+    :rtype: numpy.ndarray
+    """
+    min_y_vals = numpy.min(numpy.squeeze(hlines)[:, 1::2], axis=1)
+    max_y_vals = numpy.max(numpy.squeeze(hlines)[:, 1::2], axis=1)
+
+    if not reverse:
+        hsort_idxs = numpy.argsort(min_y_vals, axis=0)
+        hsort_idxs = list(hsort_idxs)
+        if cutoff is not None:
+            hsort_idxs = [i for i in hsort_idxs if max_y_vals[i] < cutoff]
+    else:
+        hsort_idxs = numpy.argsort(max_y_vals, axis=0)
+        hsort_idxs = list(reversed(hsort_idxs))
+        if cutoff is not None:
+            hsort_idxs = [i for i in hsort_idxs if min_y_vals[i] > cutoff]
+
+    return hsort_idxs
 
 
 # Properties
@@ -273,8 +545,27 @@ def annotate_line(
     :param color: The BGR annotation color
     :type color: Tuple[int, int, int]
     """
+    color = tuple(map(int, color))
     points_list = numpy.intp([points])
     cv2.polylines(image, points_list, True, color, 2)
+
+
+def annotate_lines(
+    image: numpy.ndarray,
+    points_list: List[List[Tuple[int, int]]],
+    color: Tuple[int, int, int] = BLUE,
+):
+    """Annotate an image in-place with a line described by a series of points
+
+    :param image: The image
+    :type image: numpy.ndarray
+    :param points_list: A list of lists of points, as (x, y) pixel coordinates
+    :type points_list: List[List[Tuple[float, float]]]
+    :param color: The BGR annotation color
+    :type color: Tuple[int, int, int]
+    """
+    color = tuple(map(int, color))
+    cv2.polylines(image, numpy.intp(points_list), True, color, 2)
 
 
 def annotate_rectangle(
@@ -295,6 +586,7 @@ def annotate_rectangle(
     :param color: The BGR annotation color
     :type color: Tuple[int, int, int]
     """
+    color = tuple(map(int, color))
     # Make sure the pixel coordinates are integers
     x0, y0, x1, y1 = map(int, xyxy)
     cv2.rectangle(image, (x0, y0), (x1, y1), color, 2)
--- painless_panes/util.py
+++ painless_panes/util.py
@@ -1,5 +1,6 @@
 import importlib.resources
 import io
+from typing import Callable, List, Tuple
 
 import cv2  # opencv
 import numpy
@@ -68,3 +69,245 @@ def bytes_io_from_opencv_image_array(image: numpy.ndarray) -> io.BytesIO:
     bytes_io = io.BytesIO(image_bytes)
     bytes_io.seek(0)
     return bytes_io
+
+
+# 2D geometry
+def line_length(line: Tuple[float, float, float, float]) -> float:
+    """Measure the length of a 2D line
+
+    :param line: A line, in xyxy format
+    :type line: Tuple[float, float, float, float]
+    :returns: The length
+    :rtype: float
+    """
+    x0, y0, x1, y1 = numpy.squeeze(line)
+    length = numpy.sqrt((x1 - x0) ** 2 + (y1 - y0) ** 2)
+    return length
+
+
+def line_angle(line: Tuple[float, float, float, float], axis: str = "y") -> float:
+    """Measure the angle of a 2D line
+
+    :param line: A line, in xyxy format
+    :type line: Tuple[float, float, float, float]
+    :param axis: The axis to measure the angle from, defaults to 'y'
+    :type axis: str, optional
+    :returns: The angle in radians
+    :rtype: float
+    """
+    x0, y0, x1, y1 = numpy.squeeze(line)
+    num = y1 - y0
+    denom = x1 - x0
+
+    # Flip numerator and denominator if measuring from the y axis
+    if axis == "y":
+        num, denom = denom, num
+
+    ratio = num / denom if not denom == 0.0 else numpy.inf
+    ang = numpy.arctan(ratio)
+    return ang
+
+
+def ray_angle_difference(ang1: float, ang2: float) -> float:
+    """Get the ray angle difference between two angles
+
+    Returns the smallest angle between the two rays. Examples:
+        -15, 15 -> 30
+        -15, 90 -> 75
+        -90, 90 -> 0
+        -83, 95 -> 2
+
+    Modified from https://gamedev.stackexchange.com/a/4472
+
+    :param ang1: The first angle in radians
+    :type ang1: float
+    :param ang2: The second angle in radians
+    :type ang2: float
+    :return: The ray angle difference
+    :rtype: float
+    """
+    return numpy.pi - abs(abs(ang1 - ang2) - numpy.pi)
+
+
+def lines_fall_on_common_ray(
+    line1: Tuple[int, int, int, int],
+    line2: Tuple[int, int, int, int],
+    ang_thresh: float = 3,
+    dist_thresh: float = 3,
+) -> bool:
+    """Find out whether two lines fall on a common ray
+
+    Lines share a common ray if they are parallel (measured by an angle threshold) and
+    nearly overlapping (measured by a perpendicular distance threshold).
+
+    :param line1: The first line
+    :type line1: Tuple[int, int, int, int]
+    :param line2: The second line
+    :type line2: Tuple[int, int, int, int]
+    :param ang_thresh: Degree angle threshold for assessing parallelity, defaults to 2
+    :type ang_thresh: float, optional
+    :param dist_thresh: Pixel distance threshold for assessing overlap, defaults to 2
+    :type dist_thresh: float, optional
+    :return: True if they fall on a common ray, False if they don't
+    :rtype: bool
+    """
+    ang_thresh *= numpy.pi / 180.0
+
+    # 0. Sort so that line A is the longer line
+    line_a, line_b = sorted([line1, line2], key=line_length, reverse=True)
+
+    # 1. Find the angle difference to assess whether the rays are parallel
+    ang_a = line_angle(line_a)
+    ang_b = line_angle(line_b)
+    ang_diff = ray_angle_difference(ang_a, ang_b)
+    are_parallel = abs(ang_diff) < ang_thresh
+
+    are_overlapping = True
+    if are_parallel:
+        # 2. Find the perpendicular distances to assess whether the rays are overlapping
+        ox, oy = numpy.squeeze(line_a)[:2]
+        for px, py in numpy.reshape(line_b, (2, 2)):
+            line_p = [ox, oy, px, py]
+            len_p = line_length(line_p)
+            ang_p = line_angle(line_p)
+            ang_diff = ray_angle_difference(ang_a, ang_p)
+            # The perpendiculr distance is (point distance) * sin(angle difference)
+            perp_dist = len_p * numpy.sin(ang_diff)
+            are_parallel &= abs(perp_dist) < dist_thresh
+
+    return are_parallel & are_overlapping
+
+
+def merge_lines(lines: numpy.ndarray) -> Tuple[float, float, float, float]:
+    r"""Merge broken or overlapping lines into a single line
+
+    Lines must fall on the same ray for this to make sense:
+
+      \             \
+                     \
+        \     =>      \   
+                       \
+          \             \
+
+    :param lines: The lines to be merged
+    :type lines: numpy.ndarray
+    :return: The resulting merged line
+    :rtype: Tuple[float, float, float, float]
+    """
+    lens = list(map(line_length, lines))
+    angs = list(map(line_angle, lines))
+    angs = [(a + numpy.pi) % numpy.pi for a in angs]
+
+    # Do a length-weighted average to find the average line angle
+    ang = sum(L * a for L, a in zip(lens, angs)) / sum(lens)
+
+    # Convert the lines to a series of points
+    points = numpy.reshape(lines, (-1, 2))
+
+    # Use the first point as the origin
+    ox, oy = points[0]
+
+    # Determine the perpendicular and parallel positions of each point along the ray,
+    # relative to the origin
+    perp_posns = []  # perpendicular positions
+    parl_posns = []  # parallel positions
+    for px, py in points:
+        line_p = [ox, oy, px, py]
+        len_p = line_length(line_p)
+        ang_p = line_angle(line_p)
+        perp_posn = len_p * numpy.sin(ang_p - ang)
+        parl_posn = len_p * numpy.cos(ang_p - ang)
+        perp_posns.append(perp_posn)
+        parl_posns.append(parl_posn)
+
+    # Get the average perpendicular position
+    perp_shift = sum(perp_posns) / len(perp_posns)
+
+    # Get the parallel starting and ending positions
+    end_posns = [min(parl_posns), max(parl_posns)]
+
+    # Apply the perpendicular shift to the origin (should be small)
+    ox = ox + perp_shift * numpy.cos(ang)
+    oy = oy - perp_shift * numpy.sin(ang)
+
+    # Get x, y coordinates for the starting and ending positions
+    end_points = []
+    for end_posn in end_posns:
+        end_x = ox + end_posn * numpy.sin(ang)
+        end_y = oy + end_posn * numpy.cos(ang)
+        end_points.extend([end_x, end_y])
+
+    assert len(end_points) == 4
+    return tuple(end_points)
+
+
+def line_pair_ray_intersection(
+    line1: numpy.ndarray, line2: numpy.ndarray
+) -> Tuple[float, float]:
+    """Find the intersection of the rays extending from a pair of lines
+
+    Formula:
+
+        x = (x2 cot(ang2) - x1 cot(ang1) - (y2 - y1)) / (cot(ang2) - cot(ang1))
+        y = (y2 tan(ang2) - y1 tan(ang1) - (x2 - x1)) / (tan(ang2) - tan(ang1))
+
+    where
+        the points are (x1, y1) and (x2, y2),
+        the angles are ang1 and ang2
+        cot() and tan() are cotangent and tangent
+
+    :param line1: The first line
+    :type line1: numpy.ndarray
+    :param line2: The second line
+    :type line2: numpy.ndarray
+    :return: The x, y coordinates of the intersection point
+    :rtype: Tuple[float, float]
+    """
+    x1, y1 = numpy.squeeze(line1)[:2]
+    x2, y2 = numpy.squeeze(line2)[:2]
+    ang1 = line_angle(line1)
+    ang2 = line_angle(line2)
+
+    # Parallel lines don't intersect, so return None
+    if ang1 == ang2:
+        return None
+
+    tan1 = numpy.tan(ang1)
+    tan2 = numpy.tan(ang2)
+    cot1 = 1. / tan1
+    cot2 = 1. / tan2
+
+    xint = (x2 * cot2 - x1 * cot1 - (y2 - y1)) / (cot2 - cot1)
+    yint = (y2 * tan2 - y1 * tan1 - (x2 - x1)) / (tan2 - tan1)
+    return xint, yint
+
+
+#  Miscellaneous
+def equivalence_partition(
+    iterable: List[object], relation: Callable
+) -> List[List[object]]:
+    """Partitions a set of objects into equivalence classes
+
+      See https://stackoverflow.com/a/38924631
+
+    :param iterable: Collection of objects to be partitioned
+    :type iterable: List[object]
+    :param relation: A function of two objects returning `True` if they are equivalent
+    :type relation: Callable
+    :return: The same objects, grouped into equivalent classes
+    :rtype: List[List[object]]
+    """
+    classes = []
+    for obj in iterable:  # for each object
+        # find the class it is in
+        found = False
+        for cls in classes:
+            # is it equivalent to this class?
+            if relation(next(iter(cls)), obj):
+                cls.append(obj)
+                found = True
+                break
+        if not found:  # it is in a new class
+            classes.append([obj])
+
+    return classes
